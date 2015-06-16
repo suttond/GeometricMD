b35b32e533d6bbb23a597b73e3b15ad522c50b08
Daniel Sutton <suttond@users.noreply.github.com>
2015-06-16 15:47:28 +0100

    Updated Physics.py to serve as atoms object modification point.

--- Physics.py
+++ Physics.py
@@ -1,11 +1,8 @@
-from ase.calculators.emt import EMT
-
-
-def define_physics():
+def define_physics(molecule):
     """
     A function wrapper used to configure the atomistic simulation environment calculator for the computation.
 
-    :return: ase.calculator -- The ASE calculator.
+    :return: ase.atoms -- The atoms object once modified.
     """
 
-    return EMT()
+    return molecule
