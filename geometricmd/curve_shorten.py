269fdefa959dba2bc38e02c7b7380f52c6d3827d
Daniel Sutton <sutton.c.daniel@gmail.com>
2015-07-14 12:09:08 +0100

    Improved length calculation. Shorter - more-efficient - code.

--- geometricmd/curve_shorten.py
+++ geometricmd/curve_shorten.py
@@ -22,84 +22,117 @@ except ImportError as e:
     quit()
 
 try:
-    from scipy.optimize import fmin_l_bfgs_b
+    from scipy.optimize import fmin_l_bfgs_b, check_grad
 except ImportError as e:
     print 'SciPy is not installed. Try to run *pip install scipy*.'
     quit()
 
 
-def length(x, start_point, end_point, number_of_inner_nodes, mass_matrix, dimension, molecule, energy):
-    """ This function computes an approximation of the length functional for local geodesics. It also provides the
-    gradient of this approximation.
+def generate_points(x, start, end, rotation_matrix, total_number_of_points, codimension):
 
-    Args:
-      x (numpy.array): The positions of the interior curve points.
-      start_point (numpy.array): The first end point of the curve.
-      end_point (numpy.array): The last end point of the curve.
-      number_of_inner_points (int): The number of nodes along the curve, less the end points.
-      mass_matrix (numpy.array): A diagonal NumPy array containing the masses of the molecular system as computed in the SimulationClient object.
-      dimension (int): The dimension of the problem. Computed from the atomistic simulation environment.
-      molecule (ase.atoms): The ASE atoms object corresponding to the molecule being simulated.
-      energy (float): The total energy of the system.
+    tangent = np.subtract(end, start)/(total_number_of_points-1)
 
-    Returns:
-      float, numpy.array: The approximate length of the curve and the corresponding gradient.
+    points = []
+    for i in xrange(total_number_of_points):
+        points.append(np.add(start, float(i)*tangent))
 
-    """
+    for i in xrange(0, len(x)/codimension):
+        unrotated_shift = np.hstack((np.zeros(1), x[i*(codimension):(i+1)*codimension]))
+        shift = rotation_matrix.dot(unrotated_shift)
+        points[i+1] = np.add(points[i+1], shift)
 
-    # Convert start_point, x and end_point into an array representing the curve.
-    curve = np.vstack((start_point, np.reshape(x, (number_of_inner_nodes, dimension)), end_point))
+    return points
 
-    # Initialise the list to contain the metric values.
+
+def compute_metric(points, metric_function):
+
     metric = []
 
-    # We pre-compute the required values of the metric and forces to minimise repeated calls
-    for point in curve:
-        # Update the positions in the molecule object
-        molecule.set_positions(convert_vector_to_atoms(point))
+    for point in points:
+        metric.append(metric_function(point))
 
-        # Compute the Maupertuis metric co-efficient for this configuration
-        if energy <= molecule.get_potential_energy():
-            logging.error('Boundary of Configuration Space Reached: Replacing with Small Number.')
-            metric_value = 1E-12
-        else:
-            metric_value = math.sqrt(2*(energy - molecule.get_potential_energy()))
+    return metric
 
-        # Insert this value and the approximate value for the gradient of the Maupertuis metric into the metric list
-        metric.append([metric_value, -convert_atoms_to_vector(molecule.get_forces())/metric_value])
 
-    # Initialise the length variable
-    l = 0.0
+def norm(x, matrix):
+    return math.sqrt(np.inner(x, matrix.dot(x)))
 
-    # For all of the nodes, less the end node...
-    for i in xrange(number_of_inner_nodes + 1):
 
-        # Add the trapezoidal rule approximation of the length functional for a line segment
-        l += math.sqrt(np.inner(np.subtract(curve[i+1], curve[i]),
-                                      mass_matrix.dot(np.subtract(curve[i+1], curve[i])))) * 0.5 \
-             * (metric[i+1][0]+metric[i][0])
+def gnorm(x, matrix):
+    norm = 2 * math.sqrt(np.inner(x, matrix.dot(x)))
+    A = (matrix + matrix.transpose())
+    return A.dot(x) / norm
 
-    # Initialize the list to store the gradient approximation
+
+def length(x, start, end, mass_matrix, rotation_matrix, total_number_of_points, codimension, metric):
+
+    points = generate_points(x, start, end, rotation_matrix, total_number_of_points, codimension)
+
+    a = compute_metric(points, metric)
+
+    n = np.subtract(points[1], points[0])
+    b = norm(n, mass_matrix)
+    c = gnorm(n, mass_matrix)
+    u = (a[1][0]+a[0][0])
+
+    l = u * b
     g = []
 
-    # For all of the interior nodes...
-    for i in xrange(1,number_of_inner_nodes + 1):
+    for i in xrange(1, len(points)-1):
 
-        # Pre-compute repeated quantities in the gradient expression
-        n_1 = np.linalg.norm(np.subtract(curve[i+1], mass_matrix.dot(curve[i])))
-        n_2 = np.linalg.norm(np.subtract(curve[i], mass_matrix.dot(curve[i-1])))
-        t_1 = mass_matrix.dot(np.subtract(curve[i+1], curve[i])) / n_1
-        t_2 = mass_matrix.dot(np.subtract(curve[i], curve[i-1])) / n_2
+        n = np.subtract(points[i+1], points[i])
+        d = norm(n, mass_matrix)
+        e = gnorm(n, mass_matrix)
+        v = (a[i+1][0]+a[i][0])
 
-        # Compute the gradient component
-        g_component = metric[i][1] * (n_1 + n_2) - (metric[i+1][0] + metric[i][0]) * t_1 \
-                      + (metric[i-1][0] + metric[i][0]) * t_2
+        l += v * norm(n, mass_matrix)
 
-        # Append this component into the gradient list
-        g.append(g_component)
+        g.append(rotation_matrix.transpose().dot(a[i][1] * (b + d) + u * c - v * e)[1:])
 
-    # Return the values of length and gradient - the gradient is flattened for dimensional consistency
-    return l, 0.5*np.asarray(g).flatten()
+        b = d
+        c = e
+        u = v
+
+    return 0.5 * l, 0.5 * np.asarray(g).flatten()
+
+
+def get_rotation(start_point, end_point, dimension):
+
+    tangent = np.subtract(end_point, start_point)
+
+        # Set the first column of our output matrix as tangent
+    mx = tangent
+
+    # Find the first non-zero entry of the tangent vector (exists as start and endpoints are different)
+    j = np.nonzero(mx)[0][0]
+
+    # For the remaining dim - 1 columns choose unit basis vectors of the form (0,...,0,1,0,...,0) with the nonzero entry
+    # not in position j.
+    for i in xrange(1, dimension):
+        if j != i:
+            e = np.zeros(dimension)
+            e[i] = 1
+            mx = np.vstack((mx, e))
+
+    mx = mx.transpose()
+
+    # With the resulting matrix, perform the Gram-Schmidt orthonormalisation procedure on the transpose of the matrix
+    # and return it.
+    m, n = np.shape(mx)
+    Q = np.zeros([m, n])
+    R = np.zeros([n, n])
+    v = np.zeros(m)
+
+    for j in range(n):
+
+        v[:] = mx[:,j]
+        for i in range(j):
+            r = np.dot(Q[:,i], mx[:,j]); R[i,j] = r
+            v[:] = v[:] - r*Q[:,i]
+        r = np.linalg.norm(v); R[j,j]= r
+        Q[:,j] = v[:]/r
+
+    return Q
 
 
 def find_geodesic_midpoint(start_point, end_point, number_of_inner_points, dimension, mass_matrix, molecule, energy,
@@ -121,22 +154,39 @@ def find_geodesic_midpoint(start_point, end_point, number_of_inner_points, dimen
 
     """
 
-    # Compute the tangent vector - the rescaled vector of the line joining the start and end points
-    tangent = (1/(float(number_of_inner_points+1)))*np.subtract(end_point, start_point)
+    def metric(point):
 
-    # Compute the initial curve, the straight line joining the start point to the end point
-    start_curve = np.asarray([np.add(start_point, tangent)], dtype='float64')
-    for i in xrange(0, int(number_of_inner_points-1)):
-        start_curve = np.concatenate((start_curve, [np.add(start_curve[i], tangent)]), axis=0)
-    start_curve = start_curve.flatten()
+        molecule.set_positions(convert_vector_to_atoms(point))
 
-    # Perform the L-BFGS method on the length functional
-    geodesic, f_min, detail = fmin_l_bfgs_b(func=length, x0=start_curve, args=(start_point, end_point,
-                                                           number_of_inner_points, mass_matrix, dimension,
-                                                           molecule, energy), pgtol=0.1)
+        cf = math.sqrt(max([2*(energy - molecule.get_potential_energy()), 1E-9]))
+
+        return [cf, molecule.get_forces().flatten()/cf]
+
+    Q = get_rotation(start_point, end_point, dimension)
+
+    geodesic, f_min, detail = fmin_l_bfgs_b(func=length,
+                                            x0=np.zeros(number_of_inner_points*(dimension-1)),
+                                            args=(start_point,
+                                                  end_point,
+                                                  mass_matrix,
+                                                  Q,
+                                                  number_of_inner_points+2,
+                                                  dimension-1,
+                                                  metric))
+
+    if detail['warnflag'] != 0:
+        print 'BFGS Warning:' + detail['task']
+
+    points = np.reshape(generate_points(geodesic, start_point, end_point, Q, number_of_inner_points+2, dimension-1),
+                        (number_of_inner_points+2, dimension))
+
+    if number_of_inner_points % 2 == 1:
+        midpoint = points[(number_of_inner_points + 1) / 2]
+    else:
+        midpoint = 0.5 * (points[number_of_inner_points / 2] + points[(number_of_inner_points / 2) + 1])
 
     # Return the node number and new midpoint
-    return [node_number, np.reshape(geodesic, (number_of_inner_points, dimension))[(number_of_inner_points + 1) / 2]]
+    return [node_number, midpoint]
 
 
 def compute_trajectory(trajectory, local_num_nodes, energy, tol, filename, configuration):
