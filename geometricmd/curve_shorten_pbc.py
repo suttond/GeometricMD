3839afe2d63ebd349b3798da8c20532b29caadc9
Daniel Sutton <sutton.c.daniel@gmail.com>
2015-07-14 15:07:42 +0100

    Simplified PBC implementation, now uses single length function.

--- geometricmd/curve_shorten_pbc.py
+++ geometricmd/curve_shorten_pbc.py
@@ -6,13 +6,12 @@ import logging
 
 # Load packages which are a part of GeometricMD
 from geometricmd.animation import write_xyz_animation
-from geometricmd.geometry import convert_vector_to_atoms, convert_atoms_to_vector
+from geometricmd.geometry import convert_vector_to_atoms
+from geometricmd.curve_shorten import generate_points, get_rotation, length
 
 # Load additional packages and check if they are installed
 try:
     import ase
-    from ase.optimize import BFGS
-    from ase.constraints import StrainFilter
 except ImportError as e:
     print 'Atomistic Simulation Environment is not installed. Try to run *pip install python-ase*.'
     quit()
@@ -30,112 +29,8 @@ except ImportError as e:
     quit()
 
 
-def length(x, start_point, end_point, start_cell, end_cell, number_of_inner_nodes, mass_matrix, dimension, molecule,
-           energy, pressure, W, epsilon=1E-12):
-    """ This function computes an approximation of the length functional for local geodesics. It also provides the
-    gradient of this approximation.
-
-    Args:
-      x (numpy.array): The positions of the interior curve points.
-      start_point (numpy.array): The first end point of the curve.
-      end_point (numpy.array): The last end point of the curve.
-      number_of_inner_points (int): The number of nodes along the curve, less the end points.
-      mass_matrix (numpy.array): A diagonal NumPy array containing the masses of the molecular system as computed in the SimulationClient object.
-      dimension (int): The dimension of the problem. Computed from the atomistic simulation environment.
-      molecule (ase.atoms): The ASE atoms object corresponding to the molecule being simulated.
-      energy (float): The total energy of the system.
-      pressure (float): The pressure of the system.
-      W (float): The stiffness of the unit cell.
-      epsilon (optional, float): A small number used where the metric co-efficient is zero or not defined.
-
-    Returns:
-      float, numpy.array: The approximate length of the curve and the corresponding gradient.
-
-    """
-
-    # Convert start_point, x and end_point into an array representing the curve.
-    eff_mass_matrix = np.vstack((np.hstack((mass_matrix, np.zeros((dimension, 9)))),
-                                 np.hstack((np.zeros((9, dimension)), np.diag(np.asarray([W] * 9))))))
-
-    x = np.hstack((start_point, x[:-number_of_inner_nodes*9], end_point, start_cell.flatten(),
-                   x[-number_of_inner_nodes*9:], end_cell.flatten()))
-
-    # Initialise the list to contain the metric values.
-    metric = []
-
-    for state_number in xrange(0,number_of_inner_nodes+2):
-        # Update the positions in the molecule object
-        cell_matrix = np.reshape(x[state_number*9+dimension*(number_of_inner_nodes+2):(state_number+1)*9+dimension*(number_of_inner_nodes+2)], (3, 3))
-        cell_volume = pressure * abs(np.linalg.det(cell_matrix))
-
-        molecule.set_positions(convert_vector_to_atoms(x[state_number*dimension:(state_number+1)*dimension]))
-        molecule.set_cell(cell_matrix)
-
-        val = molecule.get_potential_energy() + cell_volume
-
-        # Compute the Maupertuis metric co-efficient for this configuration
-        if energy <= val:
-            logging.error('Boundary of Configuration Space Reached: Replacing with Small Number.')
-            metric_value = epsilon
-        else:
-            metric_value = math.sqrt(2*(energy - val))
-
-        # Compute gradient of metric co-efficient
-        metric_gradient = -convert_atoms_to_vector(molecule.get_forces()) / metric_value
-
-        grad_volume = -cell_volume * np.linalg.inv(cell_matrix).transpose().flatten() / metric_value
-
-        # Insert this value and the approximate value for the gradient of the Maupertuis metric into the metric list
-        metric.append([metric_value, np.hstack((metric_gradient, grad_volume))])
-
-    def state(i):
-        return np.hstack((x[(i)*dimension:(i+1)*dimension],
-                          x[i*9+dimension*(number_of_inner_nodes+2):(i+1)*9+dimension*(number_of_inner_nodes+2)]))
-
-    def mass_norm(vector):
-        return math.sqrt(np.inner(vector, eff_mass_matrix.dot(vector)))
-
-    # Initialise the length variable
-    n_1 = mass_norm(np.subtract(state(1), state(0)))
-    t_1 = eff_mass_matrix.dot(np.subtract(state(1), state(0))) / n_1
-    m_1 = metric[1][0] + metric[0][0]
-    l = n_1 * m_1
-
-
-    # Initialize the list to store the gradient approximation
-    g = np.empty((number_of_inner_nodes, dimension))
-    c = np.empty((number_of_inner_nodes, 9))
-
-    # For all of the interior nodes...
-    for i in xrange(1, number_of_inner_nodes + 1):
-
-        # Pre-compute repeated quantities in the gradient expression
-        n_2 = mass_norm(np.subtract(state(i+1), state(i)))
-        t_2 = eff_mass_matrix.dot(np.subtract(state(i+1), state(i))) / n_2
-        m_2 = metric[i+1][0] + metric[i][0]
-
-        # Compute length contribution
-        l += n_2 * m_2
-
-        # Compute the gradient component
-        g_component = metric[i][1] * (n_2 + n_1) - m_2 * t_2 + m_1 * t_1
-
-        # Append this component into the gradient list
-
-        g[i-1] = g_component[:-9]
-        c[i-1] = g_component[-9:]
-
-        n_1 = n_2
-        t_1 = t_2
-        m_1 = m_2
-
-    # Return the values of length and gradient - the gradient is flattened for dimensional consistency
-
-    return 0.5*l, 0.5*np.hstack((g.flatten(), c.flatten()))
-
-
-def find_geodesic_midpoint(start_point, end_point, start_cell, end_cell, number_of_inner_points, dimension,
-                           mass_matrix, molecule, energy, node_number, pressure, W):
+def find_geodesic_midpoint(start_point, end_point, start_cell, end_cell, number_of_inner_points, dimension, mass_matrix,
+                           molecule, energy, node_number, length_function, W, pressure):
     """ This function computes the local geodesic curve joining start_point to end_point using the L-BFGS method.
 
     Args:
@@ -147,57 +42,69 @@ def find_geodesic_midpoint(start_point, end_point, start_cell, end_cell, number_
       molecule (ase.atoms): The ASE atoms object corresponding to the molecule being simulated.
       energy (float): The total energy of the system.
       node_number (int): The node number for which we are calculating a new position for.
-      pressure (float): The pressure of the system.
-      W (float): The stiffness of the unit cell.
 
     Returns:
       numpy.array: The midpoint along the approximate local geodesic curve.
 
     """
 
-    # Compute the tangent vector - the rescaled vector of the line joining the start and end points
-    tangent = (1/(float(number_of_inner_points+1)))*np.subtract(end_point, start_point)
+    def metric(point):
 
-    # Compute the initial curve, the straight line joining the start point to the end point
-    start_curve = np.asarray([np.add(start_point, tangent)], dtype='float64')
-    for i in xrange(0, int(number_of_inner_points-1)):
-        start_curve = np.concatenate((start_curve, [np.add(start_curve[i], tangent)]), axis=0)
-    start_curve = start_curve.flatten()
+        molecule.set_positions(convert_vector_to_atoms(point[:-9]))
+        minus_grad_V = molecule.get_forces().flatten()
 
-    # Compute the cell tangent vector - the rescaled vector of the line joining the start and end points
-    cell_tangent = (1/(float(number_of_inner_points)-1))*np.subtract(end_cell, start_cell)
+        cell = np.reshape(convert_vector_to_atoms(point[-9:]), (3, 3))
+        cell_volume = pressure * abs(np.linalg.det(cell))
+        grad_cell_volume = -cell_volume * np.linalg.inv(cell).transpose().flatten()
 
-    # Compute the cells, the straight line joining the start point to the end point
-    cells = [np.add(start_cell, cell_tangent)]
-    for i in xrange(0, int(number_of_inner_points-1)):
-        cells.append(np.add(cells[i], cell_tangent))
-    cells = np.asarray(cells).flatten()
+        cf = math.sqrt(max([2*(energy - molecule.get_potential_energy() - cell_volume), 1E-9]))
 
-    x0 = np.hstack((start_curve, cells))
+        return [cf, np.hstack((minus_grad_V, grad_cell_volume))/cf]
 
-    # Perform the L-BFGS method on the length functional
-    geodesic, f_min, detail = fmin_l_bfgs_b(func=length, x0=x0, args=(start_point, end_point, start_cell, end_cell,
-                                                           number_of_inner_points, mass_matrix, dimension,
-                                                           molecule, energy, pressure, W))
+    start = np.hstack((start_point, start_cell.flatten()))
+    end = np.hstack((end_point, end_cell.flatten()))
 
-    flat_mid_cell = np.reshape(geodesic[-number_of_inner_points*9:],
-                               (number_of_inner_points, 9))[(number_of_inner_points + 1) / 2]
+    Q = get_rotation(start, end, dimension + 9)
+
+    mass_matrix = np.vstack((np.hstack((mass_matrix, np.zeros((dimension, 9)))),
+                                 np.hstack((np.zeros((9, dimension)), np.diag(np.asarray([W] * 9))))))
+
+    geodesic, f_min, detail = fmin_l_bfgs_b(func=length_function,
+                                            x0=np.zeros(number_of_inner_points*(dimension+8)),
+                                            args=(start,
+                                                  end,
+                                                  mass_matrix,
+                                                  Q,
+                                                  number_of_inner_points+2,
+                                                  dimension+8,
+                                                  metric))
+
+    if detail['warnflag'] != 0:
+        print 'BFGS Warning:' + detail['task']
+
+    points = np.reshape(generate_points(geodesic, start, end, Q, number_of_inner_points+2, dimension+8),
+                        (number_of_inner_points+2, dimension+9))
+
+    if number_of_inner_points % 2 == 1:
+        midpoint = points[(number_of_inner_points + 1) / 2][:-9]
+        midpoint_cell = np.reshape(points[(number_of_inner_points + 1) / 2][-9:], (3, 3))
+    else:
+        midpoint = 0.5 * (points[number_of_inner_points / 2] + points[(number_of_inner_points / 2) + 1])[:-9]
+        midpoint_cell = np.reshape(0.5 * (points[number_of_inner_points / 2] +
+                                          points[(number_of_inner_points / 2) + 1])[-9:], (3, 3))
 
     # Return the node number and new midpoint
-    return [node_number, np.reshape(geodesic[:-number_of_inner_points*9],
-                                             (number_of_inner_points, dimension))[(number_of_inner_points + 1) / 2],
-            np.reshape(flat_mid_cell, (3, 3))]
+    return [node_number, midpoint, midpoint_cell]
 
 
-def compute_trajectory(trajectory, local_num_nodes, energy, pressure, W, tol, filename, configuration):
+def compute_trajectory(trajectory, local_num_nodes, energy, tol, filename, configuration, length_function=length,
+                       W=1.0, pressure=1.0):
     """ This function creates a new task to compute a geodesic midpoint and submits it to the worker pool.
 
     Args:
       trajectory (curve): A GeometricMD curve object describing the initial trajectory between start and end configurations.
       local_num_nodes (int): The number of points to use when computing the local geodesics.
       energy (float): The total energy of the system.
-      pressure (float): The pressure of the system.
-      W (float): The stiffness of the unit cell.
       tol (float): The tolerance by which if the total curve movement falls below this number then the Birkhoff method stops.
       filename (str): The filename for the output files from the simulation.
       configuration (dict): A dictionary containing additional parameters for the simulation. Accepts: 'processes' - the number of processors to use (defaults to 1), 'write_to_log' - a boolean value, if true writes to a logfile, otherwise prints to console (defaults to False) and 'save_every' - an integer indicating the program will save after every 'save_every'th iteration of the Birkhoff algorithm (defaults to 1).
@@ -245,19 +152,22 @@ def compute_trajectory(trajectory, local_num_nodes, energy, pressure, W, tol, fi
             # Iterating over each node in the trajectory find a new position based on the geodesic midpoint
             # joining it's neighbours
             for node_number in trajectory:
+
                 result = find_geodesic_midpoint(trajectory.points[node_number - 1],
-                                                                                 trajectory.points[node_number + 1],
-                                                                                 trajectory.cells[node_number - 1],
-                                                                                 trajectory.cells[node_number + 1],
-                                                                                 local_num_nodes,
-                                                                                 dimension,
-                                                                                 mass_matrix,
-                                                                                 molecule,
-                                                                                 energy,
-                                                                                 node_number,
-                                                                                 pressure,
-                                                                                 W)
-                trajectory.set_node_position(result[0], result[1], result[2])
+                                                trajectory.points[node_number + 1],
+                                                trajectory.cells[node_number - 1],
+                                                trajectory.cells[node_number + 1],
+                                                local_num_nodes,
+                                                dimension,
+                                                mass_matrix,
+                                                molecule,
+                                                energy,
+                                                node_number,
+                                                length_function,
+                                                W,
+                                                pressure)
+
+                trajectory.set_node_position(node_number, result[1], result[2])
 
             # Once all the nodes in the curve have been tested, print the node movement
             logging.info('Curve Movement: ' + str(trajectory.movement))
@@ -283,7 +193,7 @@ def compute_trajectory(trajectory, local_num_nodes, energy, pressure, W, tol, fi
             trajectory.set_node_position(result[0], result[1], result[2])
 
         # Create a pool of worker processes to work in parallel
-        pool = multiprocessing.Pool(processes=(processes-1))
+        pool = multiprocessing.Pool(processes=processes)
 
         # Main loop of the Birkhoff algorithm, continues until curve.movement < tol then breaks out
         while True:
@@ -292,9 +202,9 @@ def compute_trajectory(trajectory, local_num_nodes, energy, pressure, W, tol, fi
             # geodesic midpoint joining it's neighbours. Add this task to the pool queue.
             for node_number in trajectory:
                 pool.apply_async(func=find_geodesic_midpoint,
-                         args=(trajectory.points[node_number - 1], trajectory.points[node_number + 1], trajectory.cells[node_number - 1],
-                                                                                 trajectory.cells[node_number + 1], local_num_nodes,
-                               dimension, mass_matrix, molecule, energy, node_number,),
+                         args=(trajectory.points[node_number - 1], trajectory.points[node_number + 1],
+                               trajectory.cells[node_number - 1], trajectory.cells[node_number + 1], local_num_nodes,
+                               dimension, mass_matrix, molecule, energy, node_number, length_function, W, pressure,),
                          callback=update_curve)
 
             # If all the nodes in the trajectory have been moved...
