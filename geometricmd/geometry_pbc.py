6f77e94e5ef31bde92ceabd73b6098488a5da747
Daniel Sutton <sutton.c.daniel@gmail.com>
2015-07-16 12:26:09 +0100

    Documentation for geometry_pbc completed.

--- geometricmd/geometry_pbc.py
+++ geometricmd/geometry_pbc.py
@@ -3,11 +3,47 @@ import numpy as np
 import geometricmd.geometry
 
 class Curve(geometricmd.geometry.Curve):
+    """
+    The curve class that is part of the geometry_pbc module is a Child object of the curve class in the geometry module.
+     It extends provides all of the functionality of its parent class, however has additional attributes to handle
+    cell behaviour.
+
+    The purpose of this object is to provide a Curve object with a custom iterator allowing for the Birkhoff algorithm
+    to be applied.
+
+    In addition to the attributes of the parent class the following additional attributes are provided.
+
+    Attributes:
+      start_cell (numpy.array) :
+          A NumPy array describing the first point in the curve.
+      end_cell (numpy.array) :
+          A NumPy array describing the last point in the curve.
+      cell_tangent (numpy.array) :
+          A 'tangent' matrix allowing for the code to estimate the cell shapes for intermediate configurations.
+      cells (list) :
+          A list of cell configurations corresponding to each molecular configuration.
+
+    """
 
     def __init__(self, start_point, end_point, number_of_nodes, energy):
+        """The constructor for the Curve class.
 
+        Args:
+          start_point (ase.atoms) :
+              An ASE atoms object describing the initial state. A calculator needs to be set on this object.
+          end_point (ase.atoms) :
+              An ASE atoms object describing the final state.
+          number_of_nodes (int) :
+              The number of nodes that the curve is to consist of, including the start and end points.
+          energy (float) :
+              The total Hamiltonian energy to be used in the simulation.
+
+        """
+
+        # Call initialiser from parent class
         geometricmd.geometry.Curve.__init__(self, start_point, end_point, number_of_nodes, energy)
 
+        # Obtain and store initial and final cell shapes
         self.start_cell = start_point.get_cell()
         self.end_cell = end_point.get_cell()
 
@@ -19,17 +55,20 @@ class Curve(geometricmd.geometry.Curve):
         for i in xrange(0, int(self.number_of_nodes-1)):
             self.cells.append(np.add(self.cells[i], self.cell_tangent))
 
-
     def set_node_position(self, node_number, new_position, new_cell):
-        """ Update the position of the node at node_number to new_position. This processes the logic for releasing
-        neighbouring nodes for further computation.
+        """ Override the set_node_position method of the parent class to handle updating new cell configurations.
 
         Arguments:
-            node_number (int): The node number of the node whose position is to be updated.
-            new_position (numpy.array): The new position of the node.
+            node_number (int) :
+                The node number of the node whose position is to be updated.
+            new_position (numpy.array) :
+                The new position of the node.
+            new_cell (numpy.array) :
+                The new cell shape for the node.
 
         """
 
+        # Call method from parent class
         geometricmd.geometry.Curve.set_node_position(self, node_number, new_position)
 
         # Update new cell shape
